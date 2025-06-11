fa1166a272e8234bc5fcea0035b986cdb915c3ab
matthewdabi <mdabi1@umbc.edu>
2025-06-11 19:24:28 -0400

    Initial project setup for BudgetApp

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// Group Project Placeholder
+// Matthew Dabi - Quality Assurance
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Budgeting App Project.");
+    }
+}
